e23a1ad0579c1ca21dc32db98c63bd3f24efc27d
Rajeshwari33 <77319570+Rajeshwari33@users.noreply.github.com>
2021-01-12 14:45:23 +0530

    Update models.py

--- Components/Web/api/adbizProduct/adbizProductEngine/models.py
+++ Components/Web/api/adbizProduct/adbizProductEngine/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class ProductEngine(models.Model):
     class Meta:
         db_table = "engine_metadata"
+
+    product_engine_code = models.CharField(max_length=255, verbose_name="Product Engine Code", null=False)
